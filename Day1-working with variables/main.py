83bd4d8964d6d5e1af5c4e5bebd30d7a259532a8
Abhijeet singh <abhijeet.singh0513@gmail.com>
2025-10-13 19:07:05 +0530

    completed day1 learnt about common functions, variables and built a simple project

---
+++ Day1-working with variables/main.py
@@ -0,0 +1,28 @@
+# write your code here
+
+print('Hello World!')
+
+# new line character
+print('Hello\nwelcome to\n100DaysOfCode')
+
+# string concatenation
+print('Hello' + ' Angela')
+print('Hello' + " " + 'Angela')
+
+# taking user input
+# input('enter your name.')
+# print('hello '+input('enter your name\n')+'!')
+
+# variables
+
+# !important Note: don't try to memorise everything keep main things in mind and google or refer docs
+
+# name=input('enter your name\n')
+# print(name)
+# to print length of string
+# print(len(name))
+
+# print(len(input('enter your name\n')))
+
+# variable naming: name the variable which is easy to understand and name sense
+username='angela'
---
+++ Day1-working with variables/project1.py
@@ -0,0 +1,4 @@
+pet=input('What is your pet name?\n')
+city=input('In which city you grew up?\n')
+band_name=pet+' '+city
+print('your band name could be '+band_name)
